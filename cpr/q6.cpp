c2758dc0d416c93f7017f8eea3ef0ebdf419881e
Jack Wu <ecwu@users.noreply.github.com>
2018-02-28 11:50:37 +0800

    fix a bug

--- cpr/q6.cpp
+++ cpr/q6.cpp
@@ -9,12 +9,12 @@ int main() {
 	do{
 		printf("Enter the month: ");
 		scanf("%d", &user_month);
-	}while(user_month > MONTHS && user_month < 0);
+	}while(user_month > MONTHS || user_month <= 0);
 
 	do{
 		printf("Enter the day: ");
 		scanf("%d", &user_day);
-	}while(user_day > days_in_month[user_month] && user_day < 0);
+	}while(user_day > days_in_month[user_month - 1] || user_day < 0);
 
 	for (int i = 0; i < user_month - 1; i++){
 		accumulate += days_in_month[i];
